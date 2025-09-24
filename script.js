3937c94fcd5d33f9264d3b1053f3456ef73ab000
Arsenic337492 <arsen160510@gmail.com>
2025-09-24 23:31:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -315,7 +315,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                 </svg>
-                <span data-translate="profile">Профиль</span>
+                <span data-translate="profile">${t('profile')}</span>
             `;
             // Обновляем перевод
             updateLanguageInterface();
